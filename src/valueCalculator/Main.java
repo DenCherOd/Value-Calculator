ca9d29e8f0e7d9175420baea7c3bf320d389a7e0
Denys Cheran <dencherod@gmail.com>
2022-05-09 18:16:36 +0300

    final commit

---
+++ src/valueCalculator/Main.java
@@ -0,0 +1,8 @@
+package valueCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        ValueCalculator valueCalculator = new ValueCalculator(new double[1000]);
+        valueCalculator.doCalc();
+    }
+}
